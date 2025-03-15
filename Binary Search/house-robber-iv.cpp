e2471d7885f6937ac55a3a1822cbbbffc4b44f0b
sadnur Islam <sadnurislam2000@gmail.com>
2025-03-16 02:36:27 +0600

    solve daily problem 16/3/25

---
+++ Binary Search/house-robber-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+bool check(vector<int>&nums, int n, int k,int mid){
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        if(nums[i]<=mid){
+            cnt++;
+            i++;
+        }
+        if(cnt>=k)return true;
+    }
+    return false;
+}
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        int high = 1000000001;
+        int low = 1;
+        int ans = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(check(nums,n,k,mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+};
